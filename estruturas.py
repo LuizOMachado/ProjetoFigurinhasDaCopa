d014c895bce68a4e4a609c0746a06b8917ea39f0
LuizOMachado <luizotaviomachado681@gmail.com>
2026-05-28 11:06:24 -0300

    Add method to append a figurinha to the album
    Implementa método para adicionar uma figurinha ao álbum.

--- estruturas.py
+++ estruturas.py
@@ -6,5 +6,22 @@ class Album:
     def __init__(self):
         self._cabeca = None  
         self._tamanho = 0
+        
+def adicionar(self, figurinha: Figurinha):
+        """Adiciona uma nova figurinha ao final do álbum"""
+        novo_nodo = NodoLista(figurinha)
+        
+        # Se o álbum estiver vazio, a nova figurinha vira a cabeça
+        if self._cabeca is None:
+            self._cabeca = novo_nodo
+        else:
+            # Percorre a lista até achar o último nó (cujo 'proximo' é None)
+            atual = self._cabeca
+            while atual.proximo is not None:
+                atual = atual.proximo
+            # Faz o último nó apontar para o novo nó criado
+            atual.proximo = novo_nodo
+            
+        self._tamanho += 1
 
 
