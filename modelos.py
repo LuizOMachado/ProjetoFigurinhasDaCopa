97aebdb7358b057ca6eb0d987525dc42b0750570
LuizOMachado <luizotaviomachado681@gmail.com>
2026-05-28 10:30:33 -0300

    Add Figurinha class with attributes

--- modelos.py
+++ modelos.py
@@ -1,1 +1,7 @@
-
+class Figurinha:
+    def __init__(self, id_figurinha: int, nome: str, pais: str, posicao: str, raridade: str):
+        self.id = id_figurinha
+        self.nome = nome
+        self.pais = pais
+        self.posicao = posicao
+        self.raridade = raridade
