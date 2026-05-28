04588be196dd6d963a616f00d794369420946d64
LuizOMachado <luizotaviomachado681@gmail.com>
2026-05-28 11:03:07 -0300

    Update estruturas.py

--- estruturas.py
+++ estruturas.py
@@ -1,3 +1,10 @@
 from modelos import NodoLista, Figurinha
 
+class Album:
+    """Implementação do Álbum usando Lista Encadeada Simples."""
+    
+    def __init__(self):
+        self._cabeca = None  
+        self._tamanho = 0
+
 
